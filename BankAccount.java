7d92791a84cde6b4d389e751efcba67007f791c8
unknown <eguo20@stuy.edu>
2020-10-28 14:03:30 -0400

    Added Method: getBalance

--- BankAccount.java
+++ BankAccount.java
@@ -1,10 +1,14 @@
 public class BankAccount {
-  private double balance
-  private int accountID
-  private String password
+  private double balance;
+  private int accountID;
+  private String password;
 
   public BankAccount(int Username, String Passcode) {
     accountID = Username;
     password = Passcode;
+  }
+
+  public String getBalance() {
+    return ("Current Balance:"+balance);
   }
 }
