cd57d494cc33d949adb082530774c1f0c88815f5
unknown <eguo20@stuy.edu>
2020-10-28 13:52:52 -0400

    Added Constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,6 +2,9 @@ public class BankAccount {
   private double balance
   private int accountID
   private String password
-  
+
+  public BankAccount(int Username, String Passcode) {
+    accountID = Username;
+    password = Passcode;
   }
 }
